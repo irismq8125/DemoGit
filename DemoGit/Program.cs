f579b08d8c90915ce261c33f468a42b16934dad8
hv <hv@PM304-GV>
2023-08-14 20:09:12 +0700

    Add project files.

---
+++ DemoGit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao so nguyen n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (Math.Sqrt(n) % 1 == 0)
+            {
+                Console.WriteLine("{0} la so chinh phuong", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} khong la so chinh phuong", n);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
